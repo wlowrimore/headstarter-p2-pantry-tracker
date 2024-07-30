b8865284549defe21dedb01b3225c212f6005607
William Lowrimore <wlowrimore@gmail.com>
2024-07-29 20:04:20 -0500

    adds material icons package, and finishes basic hero.

--- src/app/components/HeroComponent.tsx
+++ src/app/components/HeroComponent.tsx
@@ -1,4 +1,18 @@
-import { Box, Container, Grid } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import TrackChangesOutlinedIcon from "@mui/icons-material/TrackChangesOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import Image from "next/image";
 import Hero from "../../../public/images/hero.jpg";
 
@@ -20,12 +34,121 @@ const HeroComponent = () => {
         sx={{
           width: "40%",
           color: "#D9EABE",
+          padding: "0 1rem 0 0",
         }}
       >
-        <h2>Track what you have.</h2>
-        <h2>Add what you need.</h2>
-        <h2>Get great recipe ideas.</h2>
-        <h2>All in one place!</h2>
+        <Box
+          sx={{
+            lineHeight: "2.8rem",
+            marginBottom: "1rem",
+          }}
+        >
+          <h1 style={{ fontSize: "3rem" }}>Pantry Tracker</h1>
+          <h2 style={{ fontSize: "1.7rem" }}>Pantry Management System</h2>
+        </Box>
+        <Box
+          style={{
+            width: "95%",
+            color: "#E8E7C5",
+            fontSize: "1.5rem",
+            padding: "1rem 0 0 0",
+          }}
+        >
+          <List
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <ListItem
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                gap: "1rem",
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  color: "#E8E7C5",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <TrackChangesOutlinedIcon
+                  style={{ width: "2rem", height: "2rem" }}
+                />
+                <ListItemText>
+                  <h3 style={{ fontWeight: "lighter", fontSize: "1.5rem" }}>
+                    Track Pantry Items
+                  </h3>
+                </ListItemText>
+              </ListItemIcon>
+              <ListItemIcon
+                sx={{
+                  color: "#E8E7C5",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <AddOutlinedIcon style={{ width: "2rem", height: "2rem" }} />
+                <ListItemText>
+                  <h3 style={{ fontWeight: "lighter", fontSize: "1.5rem" }}>
+                    Add New Pantry Items
+                  </h3>
+                </ListItemText>
+              </ListItemIcon>
+              <ListItemIcon
+                sx={{
+                  color: "#E8E7C5",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <CloseOutlinedIcon style={{ width: "2rem", height: "2rem" }} />
+                <ListItemText>
+                  <h3 style={{ fontWeight: "lighter", fontSize: "1.5rem" }}>
+                    Remove Pantry Items
+                  </h3>
+                </ListItemText>
+              </ListItemIcon>
+              <ListItemIcon
+                sx={{
+                  color: "#E8E7C5",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <EditOutlinedIcon style={{ width: "2rem", height: "2rem" }} />
+                <ListItemText>
+                  <h3 style={{ fontWeight: "lighter", fontSize: "1.5rem" }}>
+                    Update Pantry List
+                  </h3>
+                </ListItemText>
+              </ListItemIcon>
+              <ListItemIcon
+                sx={{
+                  color: "#E8E7C5",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "0.5rem",
+                }}
+              >
+                <SearchOutlinedIcon style={{ width: "2rem", height: "2rem" }} />
+                <ListItemText>
+                  <h3 style={{ fontWeight: "lighter", fontSize: "1.5rem" }}>
+                    Search For Recipes
+                  </h3>
+                </ListItemText>
+              </ListItemIcon>
+            </ListItem>
+          </List>
+        </Box>
       </Grid>
       <Grid
         sx={{
@@ -40,6 +163,8 @@ const HeroComponent = () => {
           style={{
             width: "100%",
             height: "100%",
+            minWidth: "50%",
+            minHeight: "50%",
             color: "green",
             borderRadius: "5rem",
             boxShadow: "0 0 1rem 0 rgba(0, 0, 0, 0.7)",
